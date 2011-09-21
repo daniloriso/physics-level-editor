b6f7d7770a17dc124ed2c7096cbac9cb17081c7c
AurelienRibon <AurelienRibon@localhost>
2011-09-21 14:42:35 +0200

    (cont'd)

---
+++ editor/src/aurelienribon/leveleditor/ui/infopanels/SelectModeDefaultInfoPanel.java
@@ -0,0 +1,50 @@
+package aurelienribon.leveleditor.ui.infopanels;
+
+import aurelienribon.leveleditor.ui.InfoPanelChild;
+import aurelienribon.leveleditor.ui.Theme;
+import java.util.List;
+
+/**
+ * @author Aurelien Ribon | http://www.aurelienribon.com/
+ */
+public class SelectModeDefaultInfoPanel extends InfoPanelChild {
+    public SelectModeDefaultInfoPanel() {
+        initComponents();
+    }
+	
+	@Override
+	public void setModels(List<Object> models) {
+	}
+	
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+
+        setOpaque(false);
+
+        jLabel1.setForeground(Theme.MAIN_ALT_FOREGROUND);
+        jLabel1.setText("<html>\n<b>Tip</b><br/>\nIf you select multiple objects of the same type, you can edit all their properties at once.");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 162, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+    }// </editor-fold>//GEN-END:initComponents
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    // End of variables declaration//GEN-END:variables
+}
